e586742adc8df67fc7623dfd8955fd881473173e
larentoun <31931237+larentoun@users.noreply.github.com>
2025-10-02 15:47:38 +0900

    add pwa and service worker?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa' // Импорт плагина
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifestFilename: "manifest.json",
+    })
+  ],
 })
